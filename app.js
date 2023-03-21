ad20bf82fdbe092cb75b889434f0e9e304f27f55
Karolina Redden <dubajkaro@gmail.com>
2023-03-21 14:48:14 -0300

    destructure a json response and assign it to results variable

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ const app = Vue.createApp({
     }
   },
   methods: {
-    getUser() {
-      this.firstName = 'Sam'
-      this.lastName = "Smith"
-      this.email = 'john@email.com'
-      this.gender = 'female'
-      this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api/')
+      const { results } = await res.json()
+      console.log(results)
+
+     
+
+    
+
+
+      // this.firstName = 'Sam'
+      // this.lastName = "Smith"
+      // this.email = 'john@email.com'
+      // this.gender = 'female'
+      // this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
     },
   },
 })
