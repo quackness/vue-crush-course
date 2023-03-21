331fbe405a0850cbc9146cd8ea8d8e981590706f
Karolina Redden <dubajkaro@gmail.com>
2023-03-21 14:53:36 -0300

    link data from api with variables in the front end so they are displayed correctly

--- app.js
+++ app.js
@@ -13,17 +13,11 @@ const app = Vue.createApp({
       const res = await fetch('https://randomuser.me/api/')
       const { results } = await res.json()
       console.log(results)
-
-     
-
-    
-
-
-      // this.firstName = 'Sam'
-      // this.lastName = "Smith"
-      // this.email = 'john@email.com'
-      // this.gender = 'female'
-      // this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+      this.firstName = results[0].name.first
+      this.lastName = results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
     },
   },
 })
