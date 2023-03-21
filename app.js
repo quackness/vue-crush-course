452235ee70b08ff3834ca03fed46675243242819
Karolina Redden <dubajkaro@gmail.com>
2023-03-21 11:05:05 -0300

    bring more generic data

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const app = Vue.createApp({
-  template: '<h1>Hello {{firstname}}</h1>',
-  data() {
+ data() {
     return {
-      firstname: 'John'
+      firstname: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
 
     }
   }
