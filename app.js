6b462d9efb6fb760cd9183cfe369137814560ca6
Karolina Redden <dubajkaro@gmail.com>
2023-03-21 14:25:07 -0300

    implement event handler in methods

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = Vue.createApp({
       email: 'john@gmail.com',
       gender: 'male',
       picture: 'https://randomuser.me/api/portraits/men/10.jpg',
-
     }
-  }
+  },
+  methods: {
+    getUser() {
+      console.log(this.firstName)
+    },
+  },
 })
+
 app.mount('#app')
 
 
